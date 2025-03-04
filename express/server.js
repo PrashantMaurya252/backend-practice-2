b8368cfa4b8f86d9040420995a41084ef7ce1a6e
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-04 23:15:45 +0530

    rate limiting

--- express/server.js
+++ express/server.js
@@ -5,6 +5,8 @@ const {configureCors}  = require('./config/corsConfig.js')
 const {addTimeStamps,requestLogger} = require('./middleware/customMiddlewares.js')
 const {globalErrorhandler} = require('./middleware/errorHandler.js')
 const {urlVersioning} =require('./middleware/apiVersioning.js')
+const {createBasicRateLimiter}=require('./middleware/rateLimiting.js')
+const itemRoutes=require('./routes/item-routes.js')
 
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -16,8 +18,10 @@ app.use(addTimeStamps)
 app.use(requestLogger)
 
 app.use(configureCors())
+app.use(createBasicRateLimiter(100,15*60*1000))  // 100 request per 15 minutes
 app.use(express.json())
-app.use('/api/v1',urlVersioning('v1'))
+app.use(urlVersioning('v1'))
+app.use("/api/v1",itemRoutes)
 app.use(globalErrorhandler)
 
 app.listen(PORT,()=>{
