395b01ab18ea1f4ea48c34ac5306ae48a4157248
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-04-19 19:16:02 +0530

    postgre

---
+++ postgre/src/db/db.js
@@ -0,0 +1,26 @@
+const {Pool} = req('pg')
+
+// create a new pool instances to manage database connections
+
+// -> postgre -> :// -> [user] -> [password] -> @ -> host:port -> [database]
+
+const pool = new Pool({
+    connectionString:process.env.DATABASE_URL
+})
+
+async function query(text,params){
+    const start = Date.now()
+    try {
+        const result = await pool.query(text,params)
+
+        // execute the time
+
+        const duration = Date.now() - start
+        console.log(`Executed query: ,${{text,duration,rows:result.rowCount}}`)
+    } catch (error) {
+        console.log("Query function error",error)
+        throw error
+    }
+}
+
+module.exports = {query} 
