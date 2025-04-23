07b4383055fbd5ef341b50e2168ada08efc19e51
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-04-23 23:58:46 +0530

    integrating prometheus

--- postgre-prisma/src/server.js
+++ postgre-prisma/src/server.js
@@ -3,10 +3,40 @@ require('dotenv').config()
 const express = require('express')
 const authorRoutes = require('./routes/authorRoutes')
 const bookRoutes = require('./routes/bookRoutes')
+const promClient = require('prom-client')
 
 const app = express()
 app.use(express.json())
 
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({register})
+const httpRequestsCounter = new promClient.Counter({
+    name: "http_requests_total",
+    help:"Total number of HTTP requests",
+    labelNames:["method","route","status"]
+})
+
+register.registerMetric(httpRequestsCounter)
+
+// Middle ware to track api requests
+
+app.use((req,res,next)=>{
+    res.on('finish',()=>{
+        httpRequestsCounter.inc({
+            method: req.method,
+            route: req.path,
+            status : res.statusCode
+        })
+    })
+    next();
+})
+
+// Expose the /metrics endpoint for prometheus
+
+app.get('/metrics',async(req,res)=>{
+    res.set('Content-Type',register.contentType)
+    res.end(await register.metrics())
+})
 const PORT = process.env.PORT || 3000
 app.use('/api/author',authorRoutes)
 app.use("/api/book",bookRoutes)
