8900b43bc5d7d02471ed9944de42884b632efdb7
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-04-25 22:53:21 +0530

    nestjs concepts

--- nestjs/concepts/src/blog/blog.controller.ts
+++ nestjs/concepts/src/blog/blog.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { BlogService } from './blog.service';
 
+//  /blog -> findAll method from blog service
+// /blog/:id -> findById method from blog service
+// /blog/key/:key -> findByUniqueKey
 @Controller('blog')
 export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
   @Get()
   findAll() {
-    return 'This will return all the blogs';
+    return this.blogService.findAll();
+  }
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.blogService.findId(+id);
+  }
+
+  @Get('by-unique-key/:key')
+  findByBlogUniqueKey(@Param('key') key: string) {
+    return this.blogService.findByUniqueKey(key);
   }
 }
