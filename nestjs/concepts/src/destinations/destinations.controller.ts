58abc951982bf8d834a0c2335162aececbef78d3
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-04-27 20:55:32 +0530

    destination controller

--- nestjs/concepts/src/destinations/destinations.controller.ts
+++ nestjs/concepts/src/destinations/destinations.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { DestinationsService } from './destinations.service';
+import { CreateDestinationDto } from 'src/auth/dto/create-destination.dto';
 
 @Controller('destinations')
-export class DestinationsController {}
+@UseGuards(JwtAuthGuard)
+export class DestinationsController {
+    constructor(private readonly destinationsService:DestinationsService){}
+
+    @Post()
+    create(@Request() req, @Body() createDestinationDto:CreateDestinationDto){
+        return this.destinationsService.create(req.user.userId,createDestinationDto)
+    }
+}
--- nestjs/concepts/src/destinations/destinations.service.ts
+++ nestjs/concepts/src/destinations/destinations.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateDestinationDto } from 'src/auth/dto/create-destination.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class DestinationsService {}
+export class DestinationsService {
+    constructor(private prisma : PrismaService){}
+    async create(userId:number,createDestinationDto:CreateDestinationDto){
+        return this.prisma.destination.create({
+            data:{
+                ...createDestinationDto,
+                travelDate:new Date(createDestinationDto.travelDate).toISOString(),
+                userId
+            }
+        })
+    }
+
+    async findAll(userId)
+}
