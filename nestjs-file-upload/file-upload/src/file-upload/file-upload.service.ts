f9dab8d500fc3ebd1633d25857d457ce690e4595
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-04-29 23:21:35 +0530

    multer,cloudinary and file interceptor adding

--- nestjs-file-upload/file-upload/src/file-upload/file-upload.service.ts
+++ nestjs-file-upload/file-upload/src/file-upload/file-upload.service.ts
@@ -1,16 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import {v2 as cloudinary} from 'cloudinary'
-
+import { v2 as cloudinary } from 'cloudinary';
+import * as fs from 'fs'
 @Injectable()
 export class FileUploadService {
-    constructor(private prisma: PrismaService){
-        cloudinary.config({
-            cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-            api_key:process.env.CLOUDINARY_API_KEY,
-            api_secret:process.env.CLOUDINARY_API_SECRET
+  constructor(private prisma: PrismaService) {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
+  async uploadFile(file:Express.Multer.File){
+    try {
+        const uploadResult = await this.uploadToCloudinary(file.path)
+        const newelySavedFile = await this.prisma.file.create({
+            data:{
+                filename:file.originalname,
+                publicId:uploadResult.public_id,
+                url:uploadResult.secure_url
+            }
+        })
+        fs.unlinkSync(file.path)
+        return newelySavedFile
+    } catch (error) {
+        // removing in case of any error from local error
+        if(file.path && fs.existsSync(file.path)){
+            fs.unlinkSync(file.path)
         }
 
-        )
+        throw new InternalServerErrorException('File upload failed ! please try again after some time')
     }
+  }
+
+  private uploadToCloudinary(filePath:string):Promise<any>{
+    return new Promise((resolve,reject)=>{
+        cloudinary.uploader.upload(filePath,(error,result)=>{
+            if(error) reject(error);
+
+            resolve(result)
+        })
+    })
+  }
 }
